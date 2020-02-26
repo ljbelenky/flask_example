c6304ce80dd5b6ec7557ddd2432e07d893a6f310
Land Belenky <ljbelenky@gmail.com>
2020-02-26 07:43:29 -0700

    with flavor/value selectors

--- app.py
+++ app.py
@@ -1,48 +1,23 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, render_template, request
-from sklearn.linear_model import LinearRegression as LR
-import matplotlib.pyplot as plt 
-import os
-from PIL import Image
-
-X = np.linspace(1,100,1001).reshape(-1,1) 
-y = 3 + 2*X + 50 * np.sin(X/5)
-
-model = LR().fit(X,y)
 
 app = Flask(__name__)
 
-@app.route('/select', methods = ['GET', 'POST'])
-def select():
-    vocab = ['fruit','berry','rose','apple', 'banana', 'aromatic','rich']
-    return render_template('select.html', vocab = vocab)
-
-@app.route('/inputs', methods = ['GET','POST'])
-def inputs():
-
-    descriptors = [request.values['description'+str(i)] for i in range(5)]
-
-    return ' '.join(descriptors)
-
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
-    return 'Hello World'
+    vocab = ['shoelace','manure','soap','formaldehyde','brimstone']
+    values = [1,2,3,4,5]
+    return render_template('select.html', vocab = vocab, values = values)
 
-@app.route('/data')
-def data():
-    plt.scatter(X,y)
-    plt.savefig('static/data.png')
-    plt.close()
+@app.route('/result', methods = ['GET','POST'])
+def inputs():
+    columns = [request.values['description'+str(i)] for i in range(5)]
+    values = [request.values['value'+str(i)] for i in range(5)]
 
-    return '<img src=static/data.png>'
+    ideal_wine = pd.DataFrame({c:[v] for c,v in zip(columns,values)})
 
-
-@app.route('/predict')
-def predict():
-    x = np.array(np.random.random()).reshape(1,1)
-    prediction = model.predict(x)
-
-    return f'{x} is predicted to be {prediction}'
+    return f'Your favorite flavor profile: {ideal_wine}. <br> Perhaps you might enjoy this wine: "X" which is was rated 85 points by Hannibal Lecter'
 
 
 if __name__ == '__main__':
