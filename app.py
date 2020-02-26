a5f3783943ecc5a180e9a00966d5a4442855757f
Land Belenky <ljbelenky@gmail.com>
2020-02-26 07:47:11 -0700

    adding some comments

--- app.py
+++ app.py
@@ -12,13 +12,15 @@ def index():
 
 @app.route('/result', methods = ['GET','POST'])
 def inputs():
-    columns = [request.values['description'+str(i)] for i in range(5)]
-    values = [request.values['value'+str(i)] for i in range(5)]
 
+# Get the description words (flavors) from request and put in a list
+    columns = [request.values['description'+str(i)] for i in range(5)]
+# Get the values from request and put in a list
+    values = [request.values['value'+str(i)] for i in range(5)]
+# Combine flavors and values to create a pandas DataFrame describing your ideal wine
     ideal_wine = pd.DataFrame({c:[v] for c,v in zip(columns,values)})
 
     return f'Your favorite flavor profile: {ideal_wine}. <br> Perhaps you might enjoy this wine: "X" which is was rated 85 points by Hannibal Lecter'
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
